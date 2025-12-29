312cf176db663b76570db14b6086c22705c3c4a3
MBlawan265 <rikici289@gmail.com>
2025-12-29 13:07:56 +0100

    Stop tracking node_modules and .next

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"node_modules\\\\react-player\\\\lib\\\\ReactPlayer.js -> ./Preview\":{\"id\":\"node_modules\\\\react-player\\\\lib\\\\ReactPlayer.js -> ./Preview\",\"files\":[\"static/chunks/reactPlayerPreview.js\"]}}"
